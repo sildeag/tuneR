91d14db37d6200b27482bb39caf5b3123f2e6da4
Sildeag <gordon@shieldaig.com>
2016-11-17 09:58:00 -0700

    Added wac2wav handling

---
+++ R/convert_wac2wav.R
@@ -0,0 +1,33 @@
+convert_wac2wav <- 
+function(wac_filename, wav_filename, overwrite = FALSE){
+    if(!is.character(wac_filename))
+        stop("'wac_filename' must be of type character.")
+    if(!is.character(wav_filename))
+        stop("'wav_filename' must be of type character.")
+    if(length(wac_filename) < 1)
+        stop("Please specify 'wac_filename'.")
+    if(length(wav_filename) < 1)
+        stop("Please specify 'wav_filename'.")
+    if(!file.exists(wac_filename))
+        stop("File '", wac_filename, "' does not exist.")
+    if(file.access(wac_filename, 4))
+        stop("No read permission for file ", wac_filename)
+    # tests to determine whether or not to overwrite 
+    if(file.exists(wav_filename) && overwrite == FALSE)
+        stop("Cannot overwrite existing file ", wav_filename)
+    if(file.exists(wav_filename) && overwrite == TRUE &&
+       file.access(wav_filename, 2) != 0)
+        stop("No write permission for file ", wav_filename)
+  ## for windows 10 64-bit environment
+  ## source("C:/users/gordo/tuneR/R/convert_wac2wav.R")
+  ## arg1 <- c("C:/users/gordo/tuneR/tests/Testfiles/test.wac")
+  ## arg2 <- c("C:/users/gordo/tuneR/tests/Testfiles/test.wav")
+    dyn.load("C:/users/gordo/tuneR/src-x64/tuneR.dll")
+    
+  ## for ubuntu 16.04 64-bit environment
+  ## source("C:/users/gordo/tuneR/R/convert_wac2wav.R")
+  ## arg1 <- c("/home/gordon/tuneR/tests/Testfiles/test.wac")
+  ## arg2 <- c("/home/gordon/tuneR/tests/Testfiles/test.wav")
+  ## dyn.load("/home/gordon/tuneR/src/wac2wav.so")
+    .Call("wac2wav", wac_filename, wav_filename,PACKAGE="tuneR")
+}
---
+++ tests/readWacTest.R
@@ -0,0 +1,19 @@
+library("tuneR")
+cwd <- getwd()
+d <- list.files(file.path(cwd,"tests","Testfiles"), pattern="\\.wac$")
+garbage <- lapply (d, 
+    function(x) {
+        #print (x)
+        wac_file <- c(file.path(cwd,"tests","Testfiles", x))
+        #print (wac_file)
+        wav_file <- gsub("c", "v", wac_file)
+        print (wav_file)
+        #cat(wav_file, "wav")
+        if(.Platform$OS.type == "unix") {
+          dyn.load(file.path(cwd,"src","wac2wav.so"))
+          } else {
+          dyn.load(file.path(cwd,"src-i386","tuneR.dll"))
+        }
+        convert_wac2wav(wac_file, wav_file, TRUE)
+        }
+ )
